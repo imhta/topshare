0b307fdd7ecc5101fcb5a940e97ecdbca21a60b2
imhta <imhtapm@gmail.com>
2019-08-21 20:53:53 +0530

    deleted url model

--- app/models/url.rb
+++
@@ -1,4 +0,0 @@
-class Url < ApplicationRecord
-    validates :url, presence: true, length: { maximum: 255 },
-    uniqueness: { case_sensitive: false }
-end
--- db/migrate/20190821150025_create_urls.rb
+++
@@ -1,9 +0,0 @@
-class CreateUrls < ActiveRecord::Migration[5.2]
-  def change
-    create_table :urls do |t|
-      t.string :url
-
-      t.timestamps
-    end
-  end
-end

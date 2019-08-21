88f29f569d5705da6b9bdcc71a593981816ccda5
imhta <imhtapm@gmail.com>
2019-08-21 21:02:17 +0530

    migrated add body to posts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_20_175212) do
+ActiveRecord::Schema.define(version: 2019_08_21_152912) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2019_08_20_175212) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "body"
   end
 
   create_table "users", force: :cascade do |t|
